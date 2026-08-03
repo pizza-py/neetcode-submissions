17603a6b6eb4f2cef9f314d3697e3820c26f7ce9
David <140508494+pizza-py@users.noreply.github.com>
2026-08-03 14:56:57 +0100

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adjMap = dict()
+        for edge in edges:
+            if edge[0] not in adjMap:
+                adjMap[edge[0]] = []
+            adjMap[edge[0]].append(edge[1])
+
+            if edge[1] not in adjMap:
+                adjMap[edge[1]] = []
+            adjMap[edge[1]].append(edge[0])
+
+        unvisited = set(adjMap)
+        def dfs(node):
+            print(unvisited)
+
+            unvisited.remove(node)
+            for thing in adjMap[node]:
+                if thing in unvisited:
+                    dfs(thing)
+        
+        res = 0
+        while unvisited:
+            res += 1
+            cur = list(unvisited)[0]
+            dfs(cur)
+        return res + n - len(adjMap)
+
+
+        
